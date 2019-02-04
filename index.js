a1d83a2cfb1f03c2881e27ff05c46121598228cc
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-02-04 23:25:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById("nested").querySelector(".target");
 }
+
+function deepestChild(){
+  var lis = document
+   .getElementById("grand-node")
+   .querySelectorAll("div");
+   var test 
+   for let( i = 0; i < lis.length-1; i++) {
+     test = lis[i].querySelector("div");
+   }
+   return test 
+}
